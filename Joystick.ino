fdbf79d3b327a6f28f4d4bfb5ea2c02fd0a9e13e
Christophe-9 <78729344+Christophe-9@users.noreply.github.com>
2021-02-08 18:19:47 +0100

    Joystick 1 LED
    Working code for 1 LED

---
+++ Joystick.ino
@@ -0,0 +1,62 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       Joystick.ino
+    Created:	08/02/2021 16:21:32
+    Author:     GA\Verha
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+
+//globale variabelen
+const int VRx = A2;
+const int VRy = A3;
+int xPosition;
+int yPosition;
+int mapXP;
+int mapYP;
+
+//LEDS
+int forward = 9;
+
+
+
+void setup()
+{
+    Serial.begin(9600);
+
+    pinMode(VRx, INPUT);
+    pinMode(VRy, INPUT);
+    pinMode(forward, OUTPUT);
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+    xPosition = analogRead(VRx);
+    yPosition = analogRead(VRy);    
+    mapXP = map(xPosition, 512, 1023, 0, 255);
+    mapYP = map(yPosition, 512, 1023, 0, 255);
+
+    Serial.print("X (1024): ");
+    Serial.print(xPosition);
+    Serial.print("| X (256): ");
+    Serial.println(mapXP);
+    //Serial.print(" | Y: ");
+    //Serial.println(mapYP);
+
+    
+    analogWrite(forward, mapXP);
+
+}
